1593adbe50fe28490209a4147a3e783f751cc82f
Tim Harshman <goteamtim+git@gmail.com>
2016-10-02 10:31:10 -0700

    Style updates

--- js/popup.js
+++ js/popup.js
@@ -23,7 +23,7 @@ function init() {
             })
         }else{
             //Use the userobject to update the UI
-            document.getElementById('hardwareStatus').innerHTML = "Your API Key: " + storedUserObject.hueUsername;
+            document.getElementById('hardwareStatus').innerHTML = "Your API Key:\n" + storedUserObject.hueUsername;
             getLights(storedUserObject);
         }
     })
@@ -37,12 +37,15 @@ function setupNewUser(ipAddress) {
             //They pressed the button
             userObject.hueUsername = response[0].success.username;
             userObject.baseApiUrl = 'http://' + userObject.localIpAddress + '/api/' + userObject.hueUsername;
-            document.getElementById('hardwareStatus').innerHTML = "Your API Key: " + userObject.hueUsername;
+            document.getElementById('hardwareStatus').innerHTML = "Your API Key: ";
+            document.getElementById('statusFromBase').text = userObject.hueUsername;
+            document.getElementById('statusFromBase').id = 'successfulKey';
             chrome.storage.local.set(userObject);
         } else if (response[0].hasOwnProperty("error")) {
             //Add button to popup that offers to run it again
             document.getElementById('hardwareStatus').innerHTML = "Hmm, something isn't right, here is the response from" + 
-            "your Hue base: " + response[0].error.description;
+            "your Hue base: ";
+            document.getElementById('statusFromBase').innerHTML = response[0].error.description;
         }
     });
     
@@ -58,6 +61,10 @@ function getLights(userInfo) {
     $.get(userInfo.baseApiUrl + "/lights", function (response) {
         console.log(response);
     })
+}
+
+function clearStorage(){
+    chrome.storage.local.clear();
 }
 
 init();
