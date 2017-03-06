8ceddac81565cc24a8c5f7973f36a1d2a0a21f80
Tim Harshman <goteamtim+git@gmail.com>
2017-03-06 08:37:29 -0800

    more gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('./node_modules/gulp');
 var jsmin = require('gulp-jsmin');
 var rename = require('gulp-rename');
+var del = require('del');
+var vinylPaths = require('vinyl-paths');
  
 gulp.task('default', function () {
     
@@ -11,8 +13,14 @@ gulp.task('test',function(){
 })
 
 gulp.task('deploy', function(){
+    gulp.src('dist/*')
+    .pipe(vinylPaths(del));
+
     gulp.src('js/popup.js')
         .pipe(jsmin())
         .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
+
+    gulp.src('manifest.json')
         .pipe(gulp.dest('dist'));
 })
